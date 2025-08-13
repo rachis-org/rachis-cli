18a5150265454b1c9ac5a36009bc621d6d0ff3cf
Oddant1 <oddant1@hotmail.com>
2025-08-13 11:31:14 -0700

    Add cache-export

--- q2cli/builtin/tools.py
+++ q2cli/builtin/tools.py
@@ -17,6 +17,43 @@ from q2cli.click.command import ToolCommand, ToolGroupCommand
 _COMBO_METAVAR = 'ARTIFACT/VISUALIZATION'
 
 
+def _export(result, output_format, output_path):
+    import distutils
+
+    import qiime2.sdk
+    import qiime2.util
+    from q2cli.core.config import CONFIG
+
+    if output_format is None:
+        if isinstance(result, qiime2.sdk.Artifact):
+            output_format = result.format.__name__
+        else:
+            output_format = 'Visualization'
+        result.export_data(output_path)
+    else:
+        if isinstance(result, qiime2.sdk.Visualization):
+            error = '--output-format cannot be used with visualizations'
+            click.echo(CONFIG.cfg_style('error', error), err=True)
+            click.get_current_context().exit(1)
+        else:
+            source = result.view(qiime2.sdk.parse_format(output_format))
+            if os.path.isfile(str(source)):
+                if os.path.isfile(output_path):
+                    os.remove(output_path)
+                elif os.path.dirname(output_path) == '':
+                    # This allows the user to pass a filename as a path if they
+                    # want their output in the current working directory
+                    output_path = os.path.join('.', output_path)
+                if os.path.dirname(output_path) != '':
+                    # create directory (recursively) if it doesn't exist yet
+                    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+                qiime2.util.duplicate(str(source), output_path)
+            else:
+                distutils.dir_util.copy_tree(str(source), output_path)
+
+    return output_format
+
+
 @click.group(help='Tools for working with QIIME 2 files.',
              cls=ToolGroupCommand)
 def tools():
@@ -54,43 +91,6 @@ def export_data(input_path, output_path, output_format):
     success = 'Exported %s as %s to %s %s' % (input_path, output_format,
                                               output_type, output_path)
     click.echo(CONFIG.cfg_style('success', success))
-
-
-def _export(result, output_format, output_path):
-    import distutils
-
-    import qiime2.sdk
-    import qiime2.util
-    from q2cli.core.config import CONFIG
-
-    if output_format is None:
-        if isinstance(result, qiime2.sdk.Artifact):
-            output_format = result.format.__name__
-        else:
-            output_format = 'Visualization'
-        result.export_data(output_path)
-    else:
-        if isinstance(result, qiime2.sdk.Visualization):
-            error = '--output-format cannot be used with visualizations'
-            click.echo(CONFIG.cfg_style('error', error), err=True)
-            click.get_current_context().exit(1)
-        else:
-            source = result.view(qiime2.sdk.parse_format(output_format))
-            if os.path.isfile(str(source)):
-                if os.path.isfile(output_path):
-                    os.remove(output_path)
-                elif os.path.dirname(output_path) == '':
-                    # This allows the user to pass a filename as a path if they
-                    # want their output in the current working directory
-                    output_path = os.path.join('.', output_path)
-                if os.path.dirname(output_path) != '':
-                    # create directory (recursively) if it doesn't exist yet
-                    os.makedirs(os.path.dirname(output_path), exist_ok=True)
-                qiime2.util.duplicate(str(source), output_path)
-            else:
-                distutils.dir_util.copy_tree(str(source), output_path)
-
-    return output_format
 
 
 def _print_descriptions(descriptions, tsv):
